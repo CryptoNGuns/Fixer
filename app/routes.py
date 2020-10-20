cfab50da5f7f74159d729fef39adf8dd013fa313
MozecieOTymZapomniec <dehneo@o2.pl>
2020-10-20 19:05:16 +0200

    template introduction

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Przemek'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Poland!'
+        },
+        {
+            'author': {'username': 'Arthur'},
+            'body': 'I dont like Micah'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+#    return render_template('index.html', user=user)

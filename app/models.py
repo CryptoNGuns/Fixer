06f3395f5b21abfe602d7aee606b84a3f15e8884
CryptoNGuns <CryptoNGuns@freememo.com>
2023-05-15 12:39:45 +0300

    added rest of lack files

--- app/models.py
+++ app/models.py
@@ -20,6 +20,7 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
     
 
 
